90b5a41dbd6e43cbdd4ee5000a4ffd112f1bc8fe
Sudirman <Sudirmancules14@gmail.com>
2019-09-26 12:15:21 +0800

    Tugas 4

--- resources/views/kategori_berita/index.blade.php
+++ resources/views/kategori_berita/index.blade.php
@@ -1,30 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header" align="center">{{ __('Kategori Berita') }}</div>
 
-<body>
+                <div class="card-body">
+                    <form>
+                        @csrf
 
-<table border= "1">
-<tr>
-<td>ID </td>
-<td>NAMA </td>
-<td>USER ID </td>
-</tr>
-
-@foreach($KategoriBerita as $item)
-
-<tr>
-
-<td>{!! $item->id !!}</td>
-<td>{!! $item->nama !!}</td>
-<td>{!! $item->users_id !!}</td>
-
-</tr>
-
-@endforeach
-
-</table>
-</body>
+                        <div class="form-group row">
+                            <table align="center" border="1">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Nama</th>
+                                    <th>ID User</th>
+                                </tr>
+                                @foreach($KategoriBerita as $item)
+                                    <tr>
+                                        <td>{{ $item->id }}</td>
+                                        <td>{{ $item->nama }}</td>
+                                        <td>{{ $item->users_id }}</td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
-
--- resources/views/pengumuman/index.blade.php
+++ resources/views/pengumuman/index.blade.php
@@ -1,35 +1,46 @@
+
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header" align="center">{{ __('Pengumuman') }}</div>
 
-<body>
+                <div class="card-body">
+                    <form>
+                        @csrf
 
-<table border= "1">
-<tr>
-<td>ID </td>	
-<td>JUDUL </td>
-<td>ISI</td>
-
-<td>USER ID </td>
-</tr>
-
-@foreach($Pengumuman as $item)
-
-<tr>
-
-
-<td>{!! $item->id !!}</td>
-<td>{!! $item->judul !!}</td>
-<td>{!! $item->isi !!}</td>
-
-<td>{!! $item->users_id !!}</td>
-
-</tr>
-
-@endforeach
-
-</table>
-</body>
+                        <div class="form-group row">
+                            <table align="center" border="1">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>JUDUL </th>
+                                    <th>ISI</th>
+                                    <th>USERS ID</th>
+                                    <th>Created AT</th>
+                                    <th>UPDATE AT</th>
+                                    <th>KATEGORI PENGUMUMAN ID</th>
+                                </tr>
+                                @foreach($Pengumuman as $item)
+                                    <tr>
+                                        <td>{{ $item->id }}</td>
+                                        <td>{{ $item->judul }}</td>
+                                        <td>{{ $item->isi }}</td>
+                                        <td>{{ $item->users_id }}</td>
+                                        <td>{{ $item->created_at }}</td>
+                                        <td>{{ $item->updated_at }}</td>
+                                        <td>{{ $item->kategori_pengumuman_id }}</td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
-
